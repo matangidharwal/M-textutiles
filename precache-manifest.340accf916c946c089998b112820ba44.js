c6e9d917fbafd436942b02a191871b37e2d459aa
matangidharwal <154001842+matangidharwal@users.noreply.github.com>
2023-12-17 15:41:43 +0530

    Updates

---
+++ precache-manifest.340accf916c946c089998b112820ba44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9459dab506a9ecd878c0fe7eb785dc69",
+    "url": "/M-textutiles/index.html"
+  },
+  {
+    "revision": "591ef40fbceab8c9eab1",
+    "url": "/M-textutiles/static/css/main.7d6be766.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/M-textutiles/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/M-textutiles/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "591ef40fbceab8c9eab1",
+    "url": "/M-textutiles/static/js/main.12922ffb.chunk.js"
+  },
+  {
+    "revision": "c109a149b7ef3a56e9af",
+    "url": "/M-textutiles/static/js/runtime~main.02843b2b.js"
+  }
+]);
